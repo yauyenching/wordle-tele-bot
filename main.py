39d63057e757a51da258a73fa5c2ee3863e530c0
Yau Yen Ching <yauyenching@pm.me>
2022-06-20 18:53:45 +0800

    Change wordle database to panda data frame

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telebot import types
 import env
 import re
 from tabulate import tabulate
+import pandas as pd
 
 API_KEY = env.API_KEY
 # print(API_KEY) # ensure that API_KEY is correctly stored
@@ -12,18 +13,18 @@ bot = telebot.TeleBot(API_KEY)
 score_dict = dict()
 
 USER_STATS = ("*Name*: {} \n"
-              "*\# of Games*: 1 \n"
-              "*Current Streak*: 1 \n"
+              "*\# of Games*: {} \n"
+              "*Current Streak*: {} \n"
               "*Average Score*: {}/6")
 
 ADDED_TEXT = ("New Wordle champion *{}* added to the leaderboard with the stats:"
               "\n\n"
-              + USER_STATS +
+              + USER_STATS.format({}, 1, 1, {}) +
               "\n\n"
               "To manually update any of these values, use /name, /games, /streak, and /average\.")
 
 class WordleScore:
-  def __init__(self, user_name, score_avg, scores, num_games, last_game, current_streak):
+  def __init__(self, user_name, edition, tries):
     """Class storing a Wordle Score.
     
     Parameters
@@ -33,20 +34,24 @@ class WordleScore:
     score_avg: int
       Total average score
     scores: dict
-      Dictionary with Wordle edition as key and # of tries as value
+      Pandas dataframe with Wordle edition as key and # of tries as value
     num_games: int
-      Total number of games tracked with bot
+      Total number of games played
     last_game: int
       Last Wordle edition played
     current_streak: int
       Current running Wordle streak
     """
     self.user_name      = user_name
-    self.score_avg      = score_avg      # updated with score message
-    self.scores         = scores         # updated with score message
-    self.num_games      = num_games      # updated with score message
-    self.last_game      = last_game      # updated with score message
-    self.current_streak = current_streak # updated with score message
+    self.score_avg      = tries    # updated with score message
+    self.num_games      = 1        # updated with score message
+    self.last_game      = edition  # updated with score message
+    self.current_streak = 1        # updated with score message
+    
+    # Define dictionary containing Wordle data
+    data = {'Edition': [edition],
+            'Tries': [tries]}
+    self.scores = pd.DataFrame(data)
     
   def setCurrentStreak(self, last_game, current_streak):
     self.last_game = self.last_game
@@ -82,7 +87,10 @@ class WordleScore:
     else:
       self.current_streak = 0
     self.last_game = edition
-    self.scores[edition] = tries
+    self.scores = self.scores.append({'Edition': edition, 'Tries': tries})
+    
+  def printStats(self):
+    USER_STATS.format(self.user_name)
 
 @bot.message_handler(commands=['greet'])
 def greet(message):
@@ -97,12 +105,7 @@ def add_score(message):
   edition = int(m.group('edition'))
   tries = int(m.group('tries'))
   if user_score == None:
-    score_dict[user_id] = WordleScore(user_name,
-                                      tries,
-                                      {edition: tries},
-                                      1,
-                                      edition,
-                                      1)
+    score_dict[user_id] = WordleScore(user_name, edition, tries)
     bot.send_message(message.chat.id, ADDED_TEXT.format(user_name, user_name, tries), parse_mode="MarkdownV2")
   else:
     user_score.updateScore(edition, tries)

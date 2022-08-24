9b7beccd4cbb1d9b5738a95e65490e49a7055c55
Yau Yen Ching <yauyenching@pm.me>
2022-08-25 03:11:40 +0800

    Third try

--- bot.py
+++ bot.py
@@ -155,4 +155,4 @@ def webhook():
     return "!", 200
     
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8455)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
